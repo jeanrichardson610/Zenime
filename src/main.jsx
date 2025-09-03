13fc90aff6aac748ac84aadb0e2ea475e83b1bd6
jeanrichardson610 <jean.richardson610@gmail.com>
2025-09-03 12:42:03 -0700

    fix: update basename and vite base for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Zenime">
+    <BrowserRouter basename="/Zenime/">
     <App />
     </BrowserRouter>
   </StrictMode>,
